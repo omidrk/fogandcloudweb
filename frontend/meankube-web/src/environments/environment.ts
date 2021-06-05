296a523cabf7f0b4a354fbf5f235c112b6f4adcd
omidrk <rkomid.5@gmail.com>
2021-06-05 11:48:58 +0200

    fixed ip and port,must change with sed

--- frontend/meankube-web/src/environments/environment.ts
+++ frontend/meankube-web/src/environments/environment.ts
@@ -2,9 +2,8 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-var ip = 'myiphere'
-var port = 'myporthere'
+
 export const environment = {
   production: false,
-  meankube_api: ("http://"+ip+":"+port+"/")
+  meankube_api: ("http://myiphere:myporthere")
 };
